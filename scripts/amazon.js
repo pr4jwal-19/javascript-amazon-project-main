26bfd10d64364ba7d01f9e2801fceaad0beffa3f
Prajwal <prajwal.dvl.2025.19@gmail.com>
2025-04-09 19:41:26 +0530

    Made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -94,6 +94,15 @@ document.querySelectorAll('.js-add-to-cart')
                     quantity: 1,
                 });
             }
-            console.log(cart);
+
+            let cartQuantity = 0;
+
+            // Update the cart count in the header
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
         });
+
     });
