299aa3aef60606c75ca2a474209579cc474fd25b
Prajwal <prajwal.dvl.2025.19@gmail.com>
2025-04-10 11:25:49 +0530

    Generated HTML for Order summary

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,13 @@
 // We load this dynamically
 export const cart = [
-    
+    {
+        id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }
 ]
 
 export function addToCart(prodId) {
