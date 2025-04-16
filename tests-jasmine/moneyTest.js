064f96cab798e1d0ff3dd36b7d89d6e391c1ee32
Prajwal <prajwal.dvl.2025.19@gmail.com>
2025-04-16 16:29:51 +0530

    created automated testing using jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from '../scripts/util/money.js';
+
+describe('test suite: formatCurrency', () => {
+    it('Converts cents into dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('Works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('Rounding off to nearest integer', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+    it('Rounding off to nearest integer', () => {
+        expect(formatCurrency(2000.4)).toEqual('20.00');
+    });
+});
