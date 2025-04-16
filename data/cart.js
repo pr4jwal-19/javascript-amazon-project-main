9fdb04592188a494a206527b29645d2dfe5c5655
Prajwal <prajwal.dvl.2025.19@gmail.com>
2025-04-16 12:19:29 +0530

    Saved data and generated HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,11 +5,13 @@ export let cart = localStorage.getItem("cart")
       {
         id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2,
+        deliveryOptionId: '1'
       },
       {
         id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 1,
-      },
+        deliveryOptionId: '2'
+      }
     ];
 
 
@@ -35,6 +37,7 @@ export function addToCart(prodId) {
         cart.push({
             id: prodId,
             quantity: 1,
+            deliveryOptionId: '1' // Default delivery option
         });
     }
 
