dcdf53a7c4796c936bc22dd37a6295885640be5b
Prajwal <prajwal.dvl.2025.19@gmail.com>
2025-04-25 10:56:20 +0530

    create loadPage async function to convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,44 @@
-import { renderOrderSummary } from './checkout/orderSummary.js';
-import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-import { loadProducts, loadProductsFetch } from '../data/products.js';
-import { loadCart } from '../data/cart.js';
+import { loadProducts, loadProductsFetch } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+// async returns a promise
+// async functions wraps the code into a Promise
+async function loadPage() {
+  console.log("Loading page...");
+
+  await loadProductsFetch(); // like -> loadProductsFetch().then(() => {})
+  const message = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve("Cart loaded");
+    });
+  });
+
+  console.log(message); // Cart loaded
+  renderOrderSummary();
+  renderPaymentSummary();
+  
+  return "200 OK";
+}
+loadPage().then((value) => {
+  console.log("Page loaded");
+  console.log(value); // 200 OK
+});
+
+/* This is the old way of loading the page using promises
+function loadPage() {
+    return new Promise((resolve) => {
+        console.log('Loading page...');
+        resolve('Page loaded');
+    });
+}
+*/
+
+// Promises and fetch
+/*
 Promise.all([
 
     loadProductsFetch(),
@@ -19,6 +53,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 // resolve -> similar to the done() function
